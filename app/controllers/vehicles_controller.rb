1118b69e914c09b204556682e98ab8c2f78367de
dannyvassallo <danielvassallo87@gmail.com>
2015-07-19 18:20:16 -0400

    setup vehicle controller and index view

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -8,20 +8,56 @@ class VehiclesController < ApplicationController
   end
 
   def new
+    @vehicle = Vehicle.new
   end
 
   def create
+    @vehicle = Vehicle.build(vehicle_params)    
+
+    if @vehicle.save
+      flash[:notice] = "Vehicle was saved."
+      redirect_to vehicle_path
+    else
+      flash[:error] = "There was an error saving the vehicle. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @vehicle = Vehicle.find(params[:id])
   end
 
   def update
+    @vehicle = Vehicle.find(params[:id])    
+
+    if @vehicle.save
+      flash[:notice] = "Vehicle was saved."
+      redirect_to vehicle_path
+    else
+      flash[:error] = "There was an error saving the vehicle. Please try again."
+      render :new
+    end    
   end
 
   def destroy
+    @vehicle = Vehicle.find(params[:id])    
+
+    if @vehicle.destroy
+      flash[:notice] = "Vehicle  was deleted successfully."
+      redirect_to @vehicle
+    else
+      flash[:error] = "There was an error deleting the vehicle. Please try again."
+      render :show
+    end
   end
 
   def show
+    @vehicle.find(params[:id])
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:year, :make, :model)
   end
 end
